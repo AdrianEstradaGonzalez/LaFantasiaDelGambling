00962f707a2818639f22c74cf36a434b651e3a82
rubenrc233 <rubenrc233@gmail.com>
2025-10-30 02:19:36 +0100

    clean ios

---
+++ frontend/clean_and_install_ios.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# Navega al directorio de ios
+cd ios
+
+# Elimina instalaciones de pods anteriores
+echo "Limpiando instalaciones de Pods anteriores..."
+rm -rf Pods Podfile.lock frontend.xcworkspace
+
+# Instala los pods
+echo "Instalando Pods..."
+pod install --repo-update
+
+echo "¡Proceso completado! Por favor, abre 'frontend.xcworkspace' en Xcode."
