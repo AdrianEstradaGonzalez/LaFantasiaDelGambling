348d4a046025f8daacb10885ee28d9d6a770d4c0
Adrián Estrada González <adrian.estrada2001@gmail.com>
2025-10-31 01:49:08 +0100

    limpio

--- backend/dist/src/env.js
+++
@@ -1,6 +0,0 @@
-export const env = {
-    PORT: Number(process.env.PORT || 3000),
-    JWT_SECRET: process.env.JWT_SECRET || "dev_secret",
-    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY || "",
-    APP_URL: process.env.APP_URL || "http://localhost:3000"
-};
